6a4bbe2e62293067e1e2d7700b16b00a32d7b55f
haris-begluk <harisbegluk@gmail.com>
2018-10-01 12:08:09 +0200

    Assigning a Claim

--- aspnetIdentityApp/Program.cs
+++ aspnetIdentityApp/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,18 @@ namespace aspnetIdentityApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
+            var username = "username@mail.com";
+            var password = "password123";
             var userStore = new UserStore<IdentityUser>();
             var userManager = new UserManager<IdentityUser>(userStore);
-            var creationResult =  userManager.Create(new IdentityUser("haris234@mail.com"), "P@ssword234");
-            Console.WriteLine("Created: {0}",creationResult.Succeeded);
+            //var creationResult =  userManager.Create(new IdentityUser(username), password);
+            //Console.WriteLine("Created: {0}",creationResult.Succeeded);  
+            var user = userManager.FindByName(username);
+            var claimResult = userManager.AddClaim(user.Id, new Claim("given_name", "Haris"));
+            Console.WriteLine("Claim: {0}", claimResult.Succeeded);
+
         }
     }
 }
