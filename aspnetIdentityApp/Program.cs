1c664f3ca9d595bb3d2a36e7303a6e021364df5d
haris-begluk <harisbegluk@gmail.com>
2018-10-01 11:57:33 +0200

    Add project files.

---
+++ aspnetIdentityApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aspnetIdentityApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+            var creationResult =  userManager.Create(new IdentityUser("haris234@mail.com"), "P@ssword234");
+            Console.WriteLine("Created: {0}",creationResult.Succeeded);
+        }
+    }
+}
